ecf8aec0d0d588687eff085bec6d9e62f94debab
Chris <tw.chrislyu@gmail.com>
2017-03-23 22:30:05 +0800

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :groups
+    resources :posts
   end
 
 
@@ -30,4 +31,5 @@ Rails.application.routes.draw do
     resources :events
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'groups#index'
 end
