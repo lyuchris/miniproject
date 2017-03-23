3730b2b011140e9cb8a79f4f15eefb3d6f4fdc52
Chris <tw.chrislyu@gmail.com>
2017-03-23 22:13:51 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,16 @@ Rails.application.routes.draw do
       post :join
       post :quit
     end
+
     resources :posts
   end
+
+  namespace :account do
+    resources :groups
+  end
+
+
+
   resources :ads do
     member do
       post 'upvote'
